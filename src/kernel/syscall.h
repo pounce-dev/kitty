2eb9f9fd747800e539b1fa065b22d003b95ae087
JimTheScientist <aidannpratt@gmail.com>
2025-05-20 17:44:50 -0600

    syscall.h update

--- src/kernel/syscall.h
+++ src/kernel/syscall.h
@@ -6,16 +6,72 @@
 #define KITTY_SYSCALL_H
 
 #include <stdbool.h>
-#include "../cpu/cpu_options.h"
 #include "stdint.h"
 
-typedef BITS_TYPE node_descriptor;
+/*
+ * bits definitions are to ensure that the parameters are
+ * consistent across all systems
+ */
 
-void open(char* path);
+typedef uint64_t node_descriptor;
+typedef uint64_t pointer;
+typedef uint64_t number;
+typedef uint64_t boolean;
+typedef uint64_t node_type;
+
+/*
+ * open(pointer path)
+ *
+ * opens a file and retrieves a node descriptor.
+ *
+ * path is a pointer to a null terminated string containing the path
+ * to the child node.
+ */
+void open(pointer path, pointer nd);
+
+/*
+ * close(node_descriptor nd)
+ *
+ * close a node, invalidating the node descriptor.
+ *
+ * nd is the node descriptor to be terminated.
+ */
 void close(node_descriptor nd);
-void read(node_descriptor nd, BITS_TYPE offset, BITS_TYPE buffer, BITS_TYPE length);
-void write(node_descriptor nd);
-void symlink(node_descriptor nd, char* path, bool writeable);
-void create(node_descriptor nd, char* path);
+
+/*
+ * read(node_descriptor nd, pointer offset, pointer buffer, number length)
+ *
+ * reads from a node, putting the data at the offset specified to the buffer
+ */
+void read(node_descriptor nd, number offset, pointer buffer, number length);
+
+/*
+ * write(node_descriptor nd, number offset, pointer buffer, number length)
+ *
+ * write to a given node.
+ *
+ * nd is the node to be written to.
+ * offset is the offset in node where the buffer will be written to.
+ * length is the length to write from.
+ */
+void write(node_descriptor nd, number offset, pointer buffer, number length);
+
+/*
+ * symlink(node_descriptor nd, pointer path, boolean writeable)
+ *
+ * nd is the node to symlink, pointer path is the
+ */
+void symlink(node_descriptor nd, pointer path, boolean writeable);
+
+/*
+ * create(pointer nd, pointer path, node_type type)
+ *
+ * nd - the pointer to the new node descriptor
+ *
+ * path - the pointer to the string with the path the new node is to be created at
+ *
+ * type - the type of the new node to create
+ */
+void create(pointer nd, pointer path, node_type type);
 
 #endif //KITTY_SYSCALL_H
